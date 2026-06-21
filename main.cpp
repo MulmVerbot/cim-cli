b965aa7d707307c16e2c70665342374f10e87197
MulmVerbot <maximilianbecker10@gmail.com>
2026-06-22 00:52:36 +0200

    0
    - Eintrag speichern (aber nur einmalig nach dem erschaffen der datei, danach ist sie einfach leer)

--- main.cpp
+++ main.cpp
@@ -1,39 +1,30 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <format>
 
 // wenn man das so macht statt direkt namespace std, kommt man viel schwieriger in schwulitäten. (also hab ich gehört)
 using std::cout;
 using std::cin;
 using std::string;
 using std::getline;
+using std::ofstream;
+using std::format;
+
+void Eingabe_Modus_an();
 
 void uebergang(){
     cout << "\n\n";
     cout << "#######################################\n";
 }
 
+
+
 void cls(){
     cout << "\033[2J\033[H"; // das hier ist die Escape suequenz (oder wie auch immer das heißt) um den Bildschirm zu clearen, und den Cursor oben links zu platzieren
-};
-
-void Eingabe_Modus_an(){
-    cls();
-    string Kunde;
-    string Problem;
-    string Info;
-
-    cout << "Kunde: ";
-    getline(cin, Kunde);
-    cout << "\nProblem: ";
-    getline(cin, Problem);
-    cout << "\nInfo: ";
-    getline(cin, Info);
-
-    cout << "\nEingegebene Daten:\n=> " << Kunde << "\n=> " << Problem << "\n=> " << Info << "\n";
-    getline(cin, Info);
-
-    
 }
+
+
 
 void Menu(string Programm_Name, string Version){
     string Menu_auswahl_e;
@@ -87,6 +78,42 @@ void Menu(string Programm_Name, string Version){
     }
 }
 
+void eintrag_speichern(string eintrag_zu_speichern){
+    ofstream datei("Eintrag.txt"); // datei ufmachen
+    if (!datei.is_open()){
+        std::cerr << "Fehler, kriege die Datei nicht auf.\n";
+        cout << "abgeschissen\n";
+    }
+
+    datei << eintrag_zu_speichern; // reinschreiben
+    cout << "Eintrag gespeichert."; // fertsch
+    uebergang();
+    Eingabe_Modus_an();
+
+}
+
+void Eingabe_Modus_an(){
+    cls();
+    string Kunde;
+    string Problem;
+    string Info;
+
+    cout << "#######################################\n";
+    cout << "Kunde: ";
+    getline(cin, Kunde);
+    cout << "#######################################\n";
+    cout << "\nProblem: ";
+    getline(cin, Problem);
+    cout << "#######################################\n";
+    cout << "\nInfo: ";
+    getline(cin, Info);
+
+    cout << "\nEingegebene Daten:\n=> " << Kunde << "\n=> " << Problem << "\n=> " << Info << "\n";
+    string Eintrag = format("#######################################\nKunde: {}\n------\nProblem: {}\n------\nInfo: {}\n#######################################", Kunde, Problem, Info);
+    eintrag_speichern(Eintrag);
+
+    
+}
 int main(){
 
 // hier unten sollen nur ein paar Variablen etc definiert werden, ähnlich wie in der Python Version.
@@ -99,3 +126,7 @@ int main(){
 
     return 0;
 }
+
+
+// um mit c++ 20 zu kompilieren und die format funktion nutzen zu können das hier bei g++ nutzen:
+// g++ -std=c++20 -O2 main.cpp -o cim-cli.exe
